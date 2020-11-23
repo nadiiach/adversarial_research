585df26910860410f26bf96cbf23f4e42c5065ed
Nadiia <nadiiachepurko@gmail.com>
2020-11-23 12:18:41 -0500

    update

--- cross_domain_transferable_perturbations/utils.py
+++ cross_domain_transferable_perturbations/utils.py
@@ -39,7 +39,9 @@ def load_gan(args, prefix, channels=3):
     print('Label: {} \t Attack: {} dependent \t Model: {} '
           '\t Distribution: {} \t Saving instance: {}'.format(args.target, args.attack_type,
                                                                args.model_type, ds, args.epochs))
-    path = _get_path(args, prefix, ds) if not args.pth_name else args.pth_name
+
+    path = _get_path(args, prefix, ds) if not args.pth_name \
+        else os.path.join("saved_models/{}".format(prefix), args.pth_name)
     #path ="/mnt/Vol2TBSabrentRoc/Projects/adversarial_research/cross_domain_transferable_perturbations/saved_models/test_frobdiff/test_frobdiff_netG_-1_img_vgg16_imagenet_0_epoch_rl.pth"
     netG.load_state_dict(torch.load(path))
 
